4ef830497556636dedbf5549127998083dc40dc6
Juan Guillermo Murillo Castillo <jg.murillo10@uniandes.edu.co>
2021-07-20 23:14:49 -0500

    Update endpoint

--- src/services/getProducts.tsx
+++ src/services/getProducts.tsx
@@ -1,4 +1,4 @@
 export function getProducts(query: string) {
-  return fetch(`http://localhost:8000/?search=${query}`)
+  return fetch(`https://us-central1-queryfy-c9513.cloudfunctions.net/query/?search=${query}`)
     .then(data => data.json());
 }
